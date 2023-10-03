bc9709aaaf2db215c55d3e08ecaccb63a7c02782
Muhammed Baqer Ahmed <moh999157@gmail.com>
2023-10-03 14:36:43 +0300

    init: permissions

--- src/permissions/dto/create-permission.dto.ts
+++ src/permissions/dto/create-permission.dto.ts
@@ -1,1 +1,10 @@
-export class CreatePermissionDto {}
+import { PickType } from '@nestjs/swagger';
+import { PermissionDto } from '../entities/permission.entity';
+
+export class CreatePermissionDto extends PickType(PermissionDto, [
+  'Account',
+  'Bank',
+  'Image',
+  'name',
+  'uniqueName',
+]) {}
--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Permissions')
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
@@ -17,18 +27,16 @@ export class PermissionsController {
     return this.permissionsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.permissionsService.findOne(+id);
-  }
-
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
-    return this.permissionsService.update(+id, updatePermissionDto);
+  update(
+    @Param('id') id: string,
+    @Body() updatePermissionDto: UpdatePermissionDto,
+  ) {
+    return this.permissionsService.update(id, updatePermissionDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.permissionsService.remove(+id);
+    return this.permissionsService.remove(id);
   }
 }
